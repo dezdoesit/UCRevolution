31cb8185f132f9fff12d87e25916c8ab6dc51a8b
Christopher <Christopher.woods723@gmail.com>
2025-03-01 14:05:41 -0500

    added detailView

---
+++ MedVision/Model/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  MedVision
+//
+//  Created by Dezmond Blair on 3/1/25.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ MedVision/Views/TrialDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  TrialDetailView.swift
+//  MedVision
+//
+//  Created by Christopher Woods on 3/1/25.
+//
+
+import SwiftUI
+
+struct TrialDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrialDetailView()
+}
