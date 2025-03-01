39ebc1b8763ca1deb620b1dd1db6ac2ad82f2388
dezdoesit <dez.blair06@gmail.com>
2025-03-01 12:22:04 -0500

    Initial Commit

---
+++ MedVision/MedVisionApp.swift
@@ -0,0 +1,33 @@
+//
+//  MedVisionApp.swift
+//  MedVision
+//
+//  Created by Dezmond Blair on 3/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MedVisionApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
